77cddd0512f61d84ce8b3e03e4107d14abcf29e0
AdityaPratapSingh <aditya.singh@daysmart.com>
2026-02-19 10:33:50 +0530

    Redirect issue brands page

--- catalog/controller/product/manufacturer.php
+++ catalog/controller/product/manufacturer.php
@@ -194,7 +194,7 @@ class ControllerProductManufacturer extends Controller {
 					'tax'         => $tax,
 					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 					'rating'      => $result['rating'],
-					'href'        => $this->url->link('product/product', 'manufacturer_id=' . $result['manufacturer_id'] . '&product_id=' . $result['product_id'] . $url)
+					'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . $url)
 				);
 			}
 
